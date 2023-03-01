472f174ee62ea793d46b4e1abc379beae1a54d34
Amanda Dias <amandadiaspedro200700@gmail.com>
2023-03-01 12:09:07 -0300

    new

--- Exercícios de fixação/desafioPaciente.py
+++ Exercícios de fixação/desafioPaciente.py
@@ -1,21 +1,19 @@
 #Caso o paciente tenha 65 anos ou mais, deve ter atendimento prioritário. Se houver suspeita de doença contagiosa, deve ir para a sala amarela. Caso seja mulher e estiver grávida, também tem direito ao atendimento prioritário. Outros casos seguirão para a sala branca e terão atendimento comum
 
 nome = input("Digite o nome: ")
-genero = input("Digite o gênero (M/F): ").upper()
 idade = int(input("Digite a idade: "))
 if idade >= 65:
-    print("Paciente com prioridade!")
-elif genero == "F":
-    if idade > 10:
-        gravidez = input("Você está grávida? (SIM/NAO): ")
-        if gravidez == "SIM":
-            print("Paciente com prioridade!")
-        else:
-            print("Paciente sem prioridade!")
-    else:
-        print("Paciente sem prioridade!")
+    print("Paciente COM prioridade!")
 else:
-    print("Paciente sem prioridade!")
+    genero = input("Digite o gênero (M/F): ").upper()
+    if genero == "F" and idade > 10:
+        gravidez = input("Você está grávida? (SIM/NAO) ").upper()
+        if gravidez == "SIM":
+            print("Paciente COM prioridade!")
+        else:
+            print("Paciente SEM prioridade")
+    else:
+        print("Paciente SEM prioridade!")
 
 doençaContagiosa = input("O paciente apresenta suspeita de doença contagiosa? (SIM/NAO) ").upper()
 if doençaContagiosa == "SIM":
