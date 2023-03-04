c892c924f4cb3442586203e199026fa02eb27510
Amanda Dias <amandadiaspedro200700@gmail.com>
2023-03-04 09:26:02 -0300

    new

---
+++ Desafios iniciais/contagemWhile.py
@@ -0,0 +1,5 @@
+num = int(input("Digite um número para iniciar a contagem até 100: "))
+while(num<=100):
+    print("\t", num)
+    num = num + 1
+print("Contagem encerrada")
--- Desafios iniciais/tabuadaFor.py
+++ Desafios iniciais/tabuadaFor.py
@@ -1,5 +1,5 @@
 # Montar uma tabuada com o número que foi digitado pelo usuário
 tabuada = int(input("Digite um número para exibir a tabuada: "))
-print("T A B U A D A")
+print("Tabuada do número", tabuada)
 for numero in range(1, 11, 1):
     print(tabuada, "x", numero, "=", tabuada*numero)
